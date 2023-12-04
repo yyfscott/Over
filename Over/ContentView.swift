5d24f250d05be61391178ddbc8f0f406b14bc5d5
Scott Yuan <scott.yuany@gmail.com>
2023-12-04 18:46:39 +0800

    Initial Commit

---
+++ Over/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Over
+//
+//  Created by Ysco7 on 2023/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Over/OverApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverApp.swift
+//  Over
+//
+//  Created by Ysco7 on 2023/12/4.
+//
+
+import SwiftUI
+
+@main
+struct OverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
