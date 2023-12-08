a21b5e53a0090c8924c1d6d0ad454564a6c968ec
Scott Yuan <scott.yuany@gmail.com>
2023-12-08 19:03:50 +0800

    Initial Commit

---
+++ Over/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  Over
+//
+//  Created by Ysco7 on 2023/12/4.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
